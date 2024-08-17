d45e479512d1d55e22331deb3551d30e12df04c2
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-16 19:47:53 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -223,7 +223,7 @@ class Game {
     
     canMoveStockToPile(toDeck, card) {
         const rankPileTop = toDeck.isEmpty() ? 13 : toDeck.peekDeck().rankCard();
-        return card.rankCard() + 1 === rankPileTop || card.rankCard() === 13;
+        return card.rankCard() + 1 === rankPileTop || card.rankCard() === 13 && toDeck.isEmpty();
     }
     
     canMovePileToSuit(fromDeck, toDeck, card) {
@@ -237,13 +237,13 @@ class Game {
     canMovePileToPile(fromDeck, toDeck, card) {
         const rankPileTop = card.rankCard();
         const rankTargetPileTop = toDeck.isEmpty() ? 13 : toDeck.peekDeck().rankCard();
-        return rankPileTop + 1 === rankTargetPileTop || rankPileTop===13;
+        return rankPileTop + 1 === rankTargetPileTop || rankPileTop===13 && toDeck.isEmpty();
     }
     
     canMoveSuitToPile(fromDeck, toDeck, card) {
         const rankSuitTop = card.rankCard();
         const rankPileTop = toDeck.isEmpty() ? 13 : toDeck.peekDeck().rankCard();
-        return rankSuitTop + 1 === rankPileTop || rankSuitTop === 13;
+        return rankSuitTop + 1 === rankPileTop || rankSuitTop === 13 && toDeck.isEmpty();
     }
 
     updateBoard() {
