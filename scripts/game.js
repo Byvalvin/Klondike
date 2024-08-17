10cc2add3b9028707ace75dc56f46f6e162d4ede
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-17 15:48:55 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -379,7 +379,7 @@ class Game {
                 console.log(deck.nameDeck().split(" ")[0]==="Pile", deck.nameDeck().split(" "));
                 if(deck.nameDeck().split(" ")[0]==="Pile"){
                     const orderedCards = [];
-                    console.log(`${deck}`);
+                    console.log(`${deck} card is:${deck.peekDeck()}`);
                     while(deck.peekDeck().isFaceup()){
                         orderedCards.unshift(deck.popDeck());
                     }
