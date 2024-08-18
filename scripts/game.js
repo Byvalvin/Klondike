1594ed6b6b8812a26e426214072f16d4ccda7d66
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-18 12:15:04 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -210,7 +210,7 @@ class Game {
         console.log("a piled mvoe");
         const [fromDeck, toDeck] = this.moveCheck(fromDeckName, toDeckName, deckCards);
 
-        const maxCardValue = toDeck().isEmpty()?  this.maxCardValue-1 : toDeck.peekDeck().rankCard();
+        const maxCardValue = toDeck.isEmpty()?  this.maxCardValue-1 : toDeck.peekDeck().rankCard();
         const moveableCards = [];
         while(!deckCards.isEmpty() && (deckCards.peekDeck().rankCard() < maxCardValue)){
             moveableCards.unshift(deckCards.popDeck());
