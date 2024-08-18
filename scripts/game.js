55639bfc7ea547e603214ad7b8c212d8ee3df9bb
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-18 14:30:06 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -223,7 +223,8 @@ class Game {
         if(stopValue!==null){
             const moveableCards = [];     
             console.log("in if", deckCards);
-            while(!deckCards.isEmpty() && (deckCards.peekDeck().rankCard()<=stopValue)){
+            while(!deckCards.isEmpty() && deckCards.peekDeck().rankCard()<=stopValue){
+                console.log(deckCards.peekDeck().rankCard(),stopValue);
                 moveableCards.push(deckCards.popDeck());
                 console.log("status",deckCards, moveableCards);
             }
