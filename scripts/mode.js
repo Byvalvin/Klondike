247f52b6f47797bb3e6be9aab13a10ce51e38f61
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-19 22:06:37 -0600

    Update mode.js

--- scripts/mode.js
+++ scripts/mode.js
@@ -1,27 +1,25 @@
 //mode.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggleButton = document.getElementById('theme-toggle');
+const themeToggleButton = document.getElementById('theme-toggle');
 
-    function applyThemeBasedOnTime() {
-        const currentHour = new Date().getHours();
-        if (currentHour >= 17) { // 17 represents 5 PM
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            const savedTheme = localStorage.getItem('theme') || 'light';
-            document.documentElement.setAttribute('data-theme', savedTheme);
-        }
+function applyThemeBasedOnTime() {
+    const currentHour = new Date().getHours();
+    if (currentHour >= 17) { // 17 represents 5 PM
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        const savedTheme = localStorage.getItem('theme') || 'light';
+        document.documentElement.setAttribute('data-theme', savedTheme);
     }
+}
 
-    // Apply theme based on time
-    applyThemeBasedOnTime();
+// Apply theme based on time
+applyThemeBasedOnTime();
 
-    // Toggle theme manually
-    themeToggleButton.addEventListener('click', () => {
-        let currentTheme = document.documentElement.getAttribute('data-theme');
-        let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    });
+// Toggle theme manually
+themeToggleButton.addEventListener('click', () => {
+    let currentTheme = document.documentElement.getAttribute('data-theme');
+    let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
 });
