30632bf8c424de9749ae5d7643379d37f8428e82
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-21 22:21:10 -0600

    Update klondike.js

--- scripts/klondike.js
+++ scripts/klondike.js
@@ -1,22 +1,3 @@
-// Function to load scripts from the stack dynamically
-const loadScripts = (stack) => {
-  if (stack.length === 0) {
-    console.log('All scripts loaded.');
-    return;
-  }
-
-  const { src, msg } = stack.shift(); // Get the next script from the stack
-
-  const script = document.createElement('script');
-  script.src = src;
-  //script.defer = true;
-  script.onload = () => {
-    console.log(msg);
-    loadScripts(stack); // Load the next script from the stack
-  };
-  script.onerror = () => console.error(`Failed to load ${src}`);
-  document.body.appendChild(script);
-};
 
 /**
  * Initializes the Klondike game and sets up event listeners for game controls.
