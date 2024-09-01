4cef0b601533dd825fc549928b6d42d17b4212dc
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-08-31 22:11:13 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -97,7 +97,7 @@ class Game {
     updateGameScore(from, to){
         if(this.SUITS.includes(to)){ // stock to suit, pile to suit
             this.score += to.sizeDeck();
-            this.score *= (this.countCompleteDecks());  
+            this.score *= (this.countCompleteDecks()+1);  
         }else if(this.SUITS.includes(from)){ // suit to pile
             this.score -= (from.sizeDeck()+1);
             this.score /= (this.countCompleteDecks());
