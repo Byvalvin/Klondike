14b7489b89c37b7d9ab433cdd71f74535e9babb7
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-09-01 15:46:29 -0600

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -286,11 +286,15 @@ class Game {
             const lines = data.split('\n').filter(line => line.trim() !== '');
             const Decks = lines.map(line => {
                 const [deckName, ...cardStrings] = line.split(' ');
-                const deck = new Deck(deckName);
-                console.log(deckName);
+                if(deckName==='Pile'){
+                    const pileNumber = cardStrings.shift();
+                    const deck = new Deck(`${deckName} ${pileNumer}`)
+                }else{
+                    const deck = new Deck(deckName);
+                }
+                console.log(deck.nameDeck());
                 cardStrings.forEach(cardString => {
                     if(cardString){
-                        console.log(cardString);
                         const cardRank = cardString[0];
                         const cardSuit = cardString[1];
                         const cardState = cardString[2];
